224dded212aafe810c24d6e486370a3d4f381347
Matthews Britto <luismatthews29@gmail.com>
2023-09-26 17:43:37 -0300

    'firstCommit'

--- App.tsx
+++ App.tsx
@@ -1,20 +1,16 @@
+import { ThemeProvider } from 'styled-components';
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Text, View } from 'react-native';
+import  theme  from '@theme/index'
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <ThemeProvider theme={ theme }>
+      <View>
+        <Text>Open up App.tsx to start working on your app!</Text>
+        <StatusBar style="auto" />
+      </View>
+    </ThemeProvider>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
