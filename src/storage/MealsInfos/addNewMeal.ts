b305ca85a9efbaa967b9b688da454f039081ec7d
Matthews Britto <luismatthews29@gmail.com>
2023-10-03 18:01:16 -0300

    iniFunctions2

--- src/storage/MealsInfos/addNewMeal.ts
+++ src/storage/MealsInfos/addNewMeal.ts
@@ -1,21 +1,31 @@
 import AsyncStorage from "@react-native-async-storage/async-storage";
 import { MealProps } from "@storage/MealsDate/MealStorageDTO";
+import { MEALS_INFOS } from "@storage/storageConfig";
 
 export async function addNewMeal( data: {} , date:string ) {
 
-      const MealObj = {
-         date: date,
-         data: data
-      }
+      // const MealObj = {
+      //    date: date,
+      //    data: data
+      // }
 
       try {
          const fetch = await AsyncStorage.getItem(date);
          
          if(fetch) {
-            return console.log(fetch)
+            // add mais informação aos dates
          }
 
-         return console.log('Não funcionou')
+         const storage = fetch ? JSON.parse(fetch) : {}
+
+         const newStorage = JSON.stringify([...storage,data])
+
+         // const stringObj = JSON.stringify(`${data}-${date}`, storage)
+
+         await AsyncStorage.setItem(`${MEALS_INFOS}-${date}`, newStorage);
+
+         console.log('talvez tenha funcionado')
+
       } catch (error) {
 
          throw error
